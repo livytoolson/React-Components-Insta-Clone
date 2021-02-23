e36fe863d2ec3c89ec3b68838022f904c0da3318
Olivia Toolson <livytoolson@gmail.com>
2021-02-23 12:49:24 -0700

    updated dummy data

--- src/dummy-data.js
+++ src/dummy-data.js
@@ -2,8 +2,8 @@ const dummyData = [
     {
         id: 1,
         username: "architecturaldigest",
-        thumbnailUrl: 'https://icon2.cleanpng.com/20180320/sqe/kisspng-twitch-computer-icons-streaming-media-youtube-live-tv-twitch-icon-5ab19172461392.001176751521586546287.jpg',
-        imageUrl: 'https://i.pinimg.com/564x/d7/98/bb/d798bb558e86019a81ccedab04e41c5f.jpg',
+        thumbnailUrl: 'https://media.licdn.com/dms/image/C560BAQGlqBjM98_xxA/company-logo_200_200/0?e=2159024400&v=beta&t=W0c5lspVUyuT_5YuozT3ambZjp76Vt27ibjWhXbRTo4',
+        imageUrl: 'https://i.pinimg.com/564x/e2/cd/f1/e2cdf17bb0eed51fc09af343d35d0cdd.jpg',
         likes: 400,
         timestamp: "July 17th 2017, 12:42:40 pm",
         comments: [
@@ -27,7 +27,7 @@ const dummyData = [
     {
         id: 2,
         username: "emmatools",
-        thumbnailUrl: 'https://icon2.cleanpng.com/20180320/sqe/kisspng-twitch-computer-icons-streaming-media-youtube-live-tv-twitch-icon-5ab19172461392.001176751521586546287.jpg',
+        thumbnailUrl: 'https://i.pinimg.com/564x/82/83/b6/8283b6c0c95e2777f602b4adabf4cca0.jpg',
         imageUrl: 'https://i.pinimg.com/564x/d7/98/bb/d798bb558e86019a81ccedab04e41c5f.jpg',
         likes: 4307,
         timestamp: "July 15th 2017, 03:12:09 pm",
@@ -57,7 +57,7 @@ const dummyData = [
     {
     id: 3,
         username: "oliviatoolson",
-        thumbnailUrl: 'https://icon2.cleanpng.com/20180320/sqe/kisspng-twitch-computer-icons-streaming-media-youtube-live-tv-twitch-icon-5ab19172461392.001176751521586546287.jpg',
+        thumbnailUrl: 'https://i.pinimg.com/564x/77/1e/90/771e90dc08d72c3cb4305512bbed4feb.jpg',
         imageUrl: 'https://i.pinimg.com/564x/10/21/5b/10215be0640b0c997ad5aefd5e2d1561.jpg',
         likes: 350,
         timestamp: "July 4th 2017, 12:42:40 pm",
